577e31053893c8891673db0bdcf87fcb0de07408
Daud Zoss <daudzoss@gmail.com>
2016-12-12 13:52:20 -0800

    optimized the kernel interaction to only exchange data when it needs to me known, double buffer stays on PU

--- playgame.c
+++ playgame.c
@@ -1,4 +1,4 @@
-// build with: gcc -o 2048 -L/usr/lib/path_to_libOpenCL.so playgame.c readkern.c -lOpenCL
+// build with: gcc -o life -L/usr/lib/path_to_libOpenCL.so playgame.c readkern.c -lcl
 
 // renamed from convolution.c in ch. 4 of "Heterogeneous Computing with OpenCL"
 
@@ -59,10 +59,10 @@ int main(int argc, char** argv) { // filename [cpu|gpu [iter [skip]]]
    // size of grid in x and y
   cl_int east_wood, M, N;
   char line[1024];
-  int i = 0;
+  int item = 0;
   const int max = 2*(2*1024*1024); // 2*2MiB (double buffer)
   int* grid = (int*) malloc(max*sizeof(int));
-  if (argc<2) { printf("%s -|file [cpu|gpu [iterations [dump]]]"); exit(argc); }
+  if (argc<2) {printf("%s file|- [iterations [skip [cpu|gpu]]]",*argv);exit(1);}
   FILE* in = strcmp(argv[1],"-") ? fopen(argv[1], "r") : NULL;
 
   // Initialize M, N and grid[] from stdin until EOF
@@ -75,15 +75,16 @@ int main(int argc, char** argv) { // filename [cpu|gpu [iter [skip]]]
 
     for (word = line; len > 30; len -= 30) {
       bin = strtob(&word, 30, "- _") << 1;
-printf("%s (%d) returned %d\n", line, len, bin);
-      grid[i] = bin;
-      if ((i += 2) > (max - 2))
+      //printf("%s (%d) returned %d\n", line, len, bin);
+      grid[item] = bin;
+      if ((item += 2) > (max - 2))
 	exit(-1);
     }
-    grid[i] = strtob(&word, len, "- _") << (31 - len);
-printf("%s (%d) returned %d\n", line, len, grid[i]);
-    i += 2;
-  }//FIXME: need to transpose if N > 30!
+    grid[item] = strtob(&word, len, "- _") << (31 - len);
+    //printf("%s (%d) returned %d\n", line, len, grid[item]);
+    item += 2;
+  }
+  //FIXME: need to transpose if N > 30!
   if (in)
     fclose(in);
   printGrid(grid, M, N);
@@ -98,12 +99,14 @@ printf("%s (%d) returned %d\n", line, len, grid[i]);
   chk(status, "clGetPlatformIDs", NULL, NULL);
 
    // Discover device
-  cl_device_id device;
-  cl_device_type pu = ((argc>1)?(('C'&*argv[1])=='C'):0) ? CL_DEVICE_TYPE_CPU
-                                                         : CL_DEVICE_TYPE_GPU;
   int iterations, printskip;
+  cl_device_id device;
+  cl_device_type pu = CL_DEVICE_TYPE_GPU;
   iterations = (argc>2) ? atoi(argv[2]) : 0;
   printskip = (argc>3) ? atoi(argv[3]) : 1;
+  printskip = ((printskip>iterations)||(printskip<1)) ? iterations : printskip;
+  if ((argc > 4) && ((argv[4][0])=='C'))
+    pu = CL_DEVICE_TYPE_CPU;
   printf("ideal workgroup size %d*%d, requesting %cPU\n", M, Nwi=(N-1+Nwi)/Nwi,
 	 (pu == CL_DEVICE_TYPE_GPU) ? 'G' : 'C');
   status = clGetDeviceIDs(platform, pu, 1, &device, NULL);
@@ -123,8 +126,8 @@ printf("%s (%d) returned %d\n", line, len, grid[i]);
   Mwg = (M-1 + Mwi)/Mwi;
   Nwi = (dims[1] < Nwi) ? dims[1] : Nwi;
   Nwg = ((N-1)/30 + Nwi)/Nwi;
-  const size_t dataSize = sizeof(int)*i; //= sizeof(int)*b(Mwg*Nwg)*(Mwi*Nwi);
-  printf("requesting %d*%d workgroups of %d*%d=%d\n", Mwg, Nwg, Mwi, Nwi, i/2);
+  const size_t dataSize = sizeof(int)*item; //= sizeof(int)*b(Mwg*Nwg)*(Mwi*Nwi);
+  printf("requesting %d*%d workgroups %d*%d=%d\n", Mwg, Nwg, Mwi, Nwi, item/2);
 
   // Create context
   cl_context_properties props[3] = {CL_CONTEXT_PLATFORM,
@@ -159,42 +162,45 @@ printf("%s (%d) returned %d\n", line, len, grid[i]);
   d_grid = clCreateBuffer(context, 0, dataSize, NULL, &status);
   chk(status, "clCreateBuffer", NULL, NULL);
 
-  for (cl_int i = 0; (iterations == 0) || (i < iterations); i++){
-    cl_int sl = 2, sh = 3, rl = 3, rh = 3;
+  for (cl_int i = 0; (iterations == 0) || (i < iterations); i++) {
+    if (i == 0) {
+      cl_int sl = 2, sh = 3, rl = 3, rh = 3;
 
-    // set arguments each time?
-    status  = clSetKernelArg(evolve, 0, sizeof(cl_mem), &d_grid);
-    status |= clSetKernelArg(evolve, 1, sizeof(cl_int), &sl);
-    status |= clSetKernelArg(evolve, 2, sizeof(cl_int), &sh);
-    status |= clSetKernelArg(evolve, 3, sizeof(cl_int), &rl);
-    status |= clSetKernelArg(evolve, 4, sizeof(cl_int), &rh);
-    status |= clSetKernelArg(evolve, 5, sizeof(cl_int), &i);
-    chk(status, "clSetKernelArg", NULL, NULL);
+      // set don't need to set arguments each time
+      status  = clSetKernelArg(evolve, 0, sizeof(cl_mem), &d_grid);
+      status |= clSetKernelArg(evolve, 1, sizeof(cl_int), &sl);
+      status |= clSetKernelArg(evolve, 2, sizeof(cl_int), &sh);
+      status |= clSetKernelArg(evolve, 3, sizeof(cl_int), &rl);
+      status |= clSetKernelArg(evolve, 4, sizeof(cl_int), &rh);
+      status |= clSetKernelArg(evolve, 5, sizeof(cl_int), &i);
+      chk(status, "clSetKernelArg", NULL, NULL);
 
-    // Copy inputs to the device: OMIT?  SHARED MEMORY?
-    status = clEnqueueWriteBuffer(queue, d_grid, CL_TRUE /*blocking_write*/,
-				  0 /*offset*/, dataSize, grid,
-				  0 /*events_in_ ...*/, NULL /*event_wait_list*/,
-				   NULL /*event*/);
-    chk(status, "clEnqueueWriteBuffer", NULL, NULL);
-
+      // don't need to send inputs to the device each time: try SHARED MEMORY?
+      status = clEnqueueWriteBuffer(queue, d_grid, CL_TRUE /*blocking_write*/,
+				    0 /*offset*/, dataSize, grid,
+				    0 /*events_in_ ...*/,
+				    NULL /*event_wait_list*/,
+				    NULL /*event*/);
+      chk(status, "clEnqueueWriteBuffer", NULL, NULL);
+    }
+    
     // Set the work item dimensions
     size_t globalSize[2] = {M, Nwg};
     size_t localSize[2] = {Mwi, 1};
     status = clEnqueueNDRangeKernel(queue, evolve, 2 /*dim*/, NULL,
 				    globalSize, localSize, 0, NULL, NULL);
     chk(status, "clEnqueueNDRangeKernel", NULL, NULL);
-
-    // Copy outputs back from the device: OMIT?  SHARED MEMORY?
-    status = clEnqueueReadBuffer(queue, d_grid, CL_TRUE /*blocking_read*/,
-				  0 /*offset*/, dataSize, grid,
-				  0 /*events_in_ ...*/, NULL /*event_wait_list*/,
-				  NULL /*event*/);
-    chk(status, "clEnqueueReadBuffer", NULL, NULL);
-
-    if (printskip && (i % printskip == 0)) {
-      printf("%d/%d:\n", i, iterations);
-      printGrid(&grid[i&1], M, N);
+    
+    if (printskip && ((i+1) % printskip == 0)) {
+      // Copy outputs back from the device: OMIT?  SHARED MEMORY?
+      status = clEnqueueReadBuffer(queue, d_grid, CL_TRUE /*blocking_read*/,
+				   0 /*offset*/, dataSize, grid,
+				   0 /*events_in_ ...*/,
+				   NULL /*event_wait_list*/,
+				   NULL /*event*/);
+      chk(status, "clEnqueueReadBuffer", NULL, NULL);
+      printf("%d/%d:\n", (i+1), iterations);
+      printGrid(&grid[i&1], M, N); //<- thus is handled the odd/even word offset
     }
   }
   exit(0);
